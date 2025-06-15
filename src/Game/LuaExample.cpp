34756970dfcfa787c285c616dee2a4a11ff02276
Sam <samboots123@gmail.com>
2025-06-15 18:48:50 +0200

    basic camera controls

--- src/Game/LuaExample.cpp
+++ src/Game/LuaExample.cpp
@@ -16,7 +16,7 @@
 
 using namespace BB;
 
-constexpr const char* free_camera = R"(
+constexpr const char* lua_scene = R"( 
     local FreeCamera = {}
 
     function FreeCamera.new(a_pos, a_forward, a_right, a_up, a_speed)
@@ -36,12 +36,12 @@ constexpr const char* free_camera = R"(
     end
 
     function FreeCamera:Move(a_x, a_y, a_z)
-        velocity = float3(0)
+        local velocity = float3(0)
         velocity = velocity + self.m_forward * a_z
         local cross = self.m_forward.Cross(self.m_up)
         velocity = velocity + cross.normalize() * a_x
         velocity = velocity + self.m_up * a_y
-        m_velocity = m_velocity + velocity * self.m_speed;
+        self.m_velocity = self.m_velocity + velocity * self.m_speed;
     end
 
     function FreeCamera:Rotate(a_yaw, a_pitch)
@@ -53,9 +53,9 @@ constexpr const char* free_camera = R"(
             self.m_pitch = -90.0
         end
         
-        local dir_x = math.cos(self.m_yaw) * cos(self.m_pitch)
+        local dir_x = math.cos(self.m_yaw) * math.cos(self.m_pitch)
         local dir_y = math.sin(self.m_pitch)
-        local dir_z = math.sin(self.m_yaw) * cos(self.m_pitch)
+        local dir_z = math.sin(self.m_yaw) * math.cos(self.m_pitch)
         local direction = float3(dir_x, dir_y, dir_z)
         
         self.m_forward = direction.Normalize()
@@ -68,17 +68,31 @@ constexpr const char* free_camera = R"(
         self.m_velocity = self.m_velocity - velocity;
         self.m_pos = self.m_pos + velocity;
     end
-)";
 
-constexpr const char* lua_scene = R"( 
-    camera = FreeCamera(float3(0, 0, 0), float3(1, 0, 0), float3(0, 1, 0), float3(0, 0, -1), 1)
+    camera = FreeCamera.new(float3(0, 0, 0), float3(1, 0, 0), float3(0, 0, -1), float3(0, 1, 0), 1)
 
     function GetCameraPos()
         return camera.m_pos
     end
 
+    function GetCameraUp()
+        return camera.m_up
+    end
+
+    function GetCameraForward()
+        return camera.m_forward
+    end
+
+    function MoveCamera(a_x, a_y, a_z)
+        camera:Move(a_x, a_y, a_z)
+    end
+
+    function RotateCamera(a_x, a_y)
+        camera:Rotate(a_x, a_y)
+    end
+
     function update(a_delta_time, selected)
-        
+        camera:Update(a_delta_time)
     end
 )";
 
@@ -90,21 +104,38 @@ bool LuaExample::Init(const uint2 a_game_viewport_size, const uint32_t a_back_bu
 
     m_context.Init(m_memory, &m_scene_hierarchy.GetECS(), gbSize);
 
-    luaL_dostring(m_context.GetState(), free_camera);
-    luaL_dostring(m_context.GetState(), lua_scene);
-
+    int status = luaL_dostring(m_context.GetState(), lua_scene);
+    BB_ASSERT(status == LUA_OK, lua_tostring(m_context.GetState(), -1));
     return true;
 }
 
 bool LuaExample::Update(const float a_delta_time, const bool a_selected)
 { 
+    const LuaStackScope scope(m_context.GetState());
+    lua_getglobal(m_context.GetState(), "update");
+    lua_pushnumber(m_context.GetState(), static_cast<lua_Number>(a_delta_time));
+    lua_pushboolean(m_context.GetState(), a_selected);
+    lua_pcall(m_context.GetState(), 2, 0, 0);
+
+    const float3 pos = GetCameraPos();
+
+    lua_getglobal(m_context.GetState(), "GetCameraUp");
+    lua_pcall(m_context.GetState(), 0, 1, 0);
+    const float3 up = *lua_getfloat3(m_context.GetState(), -1);
+    lua_getglobal(m_context.GetState(), "GetCameraForward");
+    lua_pcall(m_context.GetState(), 0, 1, 0);
+    const float3 forward = *lua_getfloat3(m_context.GetState(), -1);
+
+    const float4x4 view = Float4x4Lookat(pos, pos + forward, up);
+    m_scene_hierarchy.GetECS().GetRenderSystem().SetView(view, pos);
 
     return true;
 }
 
 float3 LuaExample::GetCameraPos() 
 {
-    LuaStackScope scope(m_context.GetState());
+    const LuaStackScope scope(m_context.GetState());
     lua_getglobal(m_context.GetState(), "GetCameraPos");
+    lua_pcall(m_context.GetState(), 0, 1, 0);
     return *lua_getfloat3(m_context.GetState(), -1);
 }
